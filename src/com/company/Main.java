c9798fae85896d22b76f18bb14795a68dae6f600
Nitish Kumar Tiwari <nitishtiwari1710@gmail.com>
2022-04-22 21:05:26 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+import java.util.Arrays;
+public class Main {
+            void gnomeSort(int[] nums) {
+                int i = 1;
+                int j = 2;
+
+                while (i < nums.length) {
+                    if (nums[i - 1] <= nums[i]) {
+                        i = j;
+                        j++;
+                    } else {
+                        int tmp = nums[i - 1];
+                        nums[i - 1] = nums[i];
+                        nums[i--] = tmp;
+                        i = (i == 0) ? j++ : i;
+                    }
+                }
+            }
+
+            // Method to test above
+            public static void main(String args[]) {
+                Main ob = new Main();
+                int nums[] = {7, -5, 3, 2, 1, 0, 45};
+                System.out.println("Original Array:");
+                System.out.println(Arrays.toString(nums));
+                ob.gnomeSort(nums);
+                System.out.println("Sorted Array");
+                System.out.println(Arrays.toString(nums));
+            }
+        }
+    
+
